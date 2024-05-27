984eb4c8940d5ac7ccdfb8feab60d29114b944d2
ItayGigi <itay.gigi1@gmail.com>
2024-05-27 16:00:45 +0300

    First broken commit

---
+++ fragment.frag
@@ -0,0 +1,138 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoord;
+uniform uvec2 Resolution;
+uniform mat4 View;
+uniform mat4 Projection;
+uniform mat4 CamRotation;
+uniform vec3 CamPosition;
+
+layout( std140 ) uniform GridBuffer
+{
+    uint Grid[64];
+};
+
+struct Ray{
+	vec3 origin;
+	vec3 dir;
+};
+
+struct Material{
+	vec3 color;
+	float smoothness;
+	float emission;
+};
+
+struct Sphere{
+	vec3 pos;
+	float radius;
+	Material mat;
+};
+
+uint GetGridCell(uint grid[8*8], int x, int y, int z){
+	if (x < 0 || x >= 8 || y < 0 || y >= 8 || z < 0 || z >= 8) return 0u;
+
+	uint row = grid[x*8+y];
+	return (row >> z*4) & 0xFu;
+}
+
+float RaySphereIntersection(Ray ray, Sphere sphere){
+	vec3 origin = ray.origin - sphere.pos;
+
+	float a = dot(ray.dir, ray.dir);
+	float b = 2*dot(origin, ray.dir);
+	float c = dot(origin, origin) - sphere.radius*sphere.radius;
+
+	float discriminant = b*b - 4*a*c;
+
+	if (discriminant < 0) return -1.;
+
+	return (-b - sqrt(discriminant))/(2*a);
+}
+
+float RaySlabIntersection(Ray ray, vec3 min, vec3 max){
+	float tmin = (min.x - ray.origin.x) / ray.dir.x; 
+	float tmax = (max.x - ray.origin.x) / ray.dir.x; 
+
+	if (tmin > tmax) {
+		float temp = tmin;
+		tmin = tmax;
+		tmax = temp;
+	}
+
+	float tymin = (min.y - ray.origin.y) / ray.dir.y; 
+	float tymax = (max.y - ray.origin.y) / ray.dir.y; 
+
+	if (tymin > tymax) {
+		float temp = tymin;
+		tymin = tymax;
+		tymax = temp;
+	}
+
+	if ((tmin > tymax) || (tymin > tmax)) 
+			return -1.; 
+
+	if (tymin > tmin) 
+			tmin = tymin; 
+
+	if (tymax < tmax) 
+			tmax = tymax; 
+
+	float tzmin = (min.z - ray.origin.z) / ray.dir.z; 
+	float tzmax = (max.z - ray.origin.z) / ray.dir.z; 
+
+	if (tzmin > tzmax) {
+		float temp = tzmin;
+		tzmin = tzmax;
+		tzmax = temp;
+	}
+
+	if ((tmin > tzmax) || (tzmin > tmax)) 
+			return -1.; 
+
+	if (tzmin > tmin) 
+			tmin = tzmin; 
+
+	if (tzmax < tmax) 
+			tmax = tzmax;
+
+	return tmin;
+}
+
+float RayGridIntersection(Ray ray){
+	float boundT = RaySlabIntersection(ray, vec3(0.), vec3(1.));
+
+	float stepx = sign(ray.dir.x);
+	float stepy = sign(ray.dir.y);
+	float stepz = sign(ray.dir.z);
+
+	return boundT;
+}
+
+void main()
+{
+	if (TexCoord.y > 0.) FragColor = vec4(GetGridCell(Grid, int(fract(TexCoord.x*4.+4.)*8.), int(TexCoord.y*32.), int(TexCoord.x * 4. + 4.))/8.);
+	else if (TexCoord.y > -1/4.) FragColor = vec4(int(fract(TexCoord.x*4.+4.)*8.)/8., int(TexCoord.y*32.+8.)/8., int(TexCoord.x * 4. + 4.)/8., 1.);
+	else if (TexCoord.y < -0.8) FragColor = vec4(Grid[int((TexCoord.x+1.)*32.)]);
+	else FragColor = vec4(0.);
+	return;
+
+	float aspect = float(Resolution.x)/float(Resolution.y);
+
+	vec3 dir = (CamRotation * vec4(TexCoord.x*aspect, TexCoord.y, 1., 1.)).xyz;
+
+	Ray ray = Ray(CamPosition, normalize(dir));
+	Sphere sphere = Sphere(vec3(0., 1., 0.), 1., Material(vec3(1.), 0., 0.));
+
+	//float dist = RaySphereIntersection(ray, sphere);
+	float dist = RaySlabIntersection(ray, vec3(0.), vec3(1.));
+
+	if (dist < 0){
+		FragColor = vec4(0., 0., 0., 1.);
+		return;
+	}
+
+	FragColor = vec4(ray.origin+ray.dir*dist, 1.0f);
+}
---
+++ main.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "shader.h"
+#include "camera.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+int windowWidth = 800, windowHeight = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = windowWidth / 2.0f;
+float lastY = windowHeight / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+float lastTitleUpdateTime = 0.f;
+
+int main() {
+	// initialize glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create window
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "My Window", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSwapInterval(0);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// initialize glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	Shader shader("vertex.vert", "fragment.frag");
+
+	// create and load vertices
+	float vertices[] = {
+		 1.0f,  1.0f, 0.0f,		1.0f, 1.0f,    // top right
+		 1.0f, -1.0f, 0.0f,		1.0f, -1.0f,   // bottom right
+		-1.0f, -1.0f, 0.0f,		-1.0f, -1.0f,  // bottom left
+		-1.0f,  1.0f, 0.0f,		-1.0f, 1.0f    // top left 
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// generate sphere grid
+	uint8_t grid[8 * 8 * 8];
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
+			for (int k = 0; k < 8; k++) grid[i * 64 + j * 8 + k] = k;//(((float)i - 3.5f) * ((float)i - 3.5f) + ((float)j - 3.5f) * ((float)j - 3.5f) + ((float)k - 3.5f) * ((float)k - 3.5f) < 16);
+
+	uint32_t data[8 * 8];
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+			data[i * 8 + j] = 
+				(grid[i * 64 + j * 8 + 0] << 0) |
+				(grid[i * 64 + j * 8 + 1] << 4) |
+				(grid[i * 64 + j * 8 + 2] << 8) |
+				(grid[i * 64 + j * 8 + 3] << 12) |
+				(grid[i * 64 + j * 8 + 4] << 16) |
+				(grid[i * 64 + j * 8 + 5] << 20) |
+				(grid[i * 64 + j * 8 + 6] << 24) |
+				(grid[i * 64 + j * 8 + 7] << 28);
+	}
+
+	for (int i = 0; i < 64; i++)
+	{
+		std::cout << std::hex << data[i] << std::endl;
+	}
+
+	unsigned int GridBuffer;
+	glGenBuffers(1, &GridBuffer);
+	glBindBuffer(GL_UNIFORM_BUFFER, GridBuffer);
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+	glBindBufferBase(GL_UNIFORM_BUFFER, 0, GridBuffer);
+	glUniformBlockBinding(shader.ID, glGetUniformLocation(shader.ID, "GridBuffer"), 0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		lastTitleUpdateTime += deltaTime;
+
+		if (lastTitleUpdateTime >= 0.1f) {
+			glfwSetWindowTitle(window, std::to_string((int)(1 / deltaTime)).c_str());
+			lastTitleUpdateTime = 0.f;
+		}
+
+		processInput(window);
+
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)windowWidth / (float)windowHeight, 0.1f, 100.0f);
+		shader.setMat4("Projection", projection);
+
+		// camera/view transformation
+		glm::mat4 view = camera.GetViewMatrix();
+		shader.setMat4("View", glm::inverse(projection * view));
+
+		shader.setMat4("CamRotation", glm::mat4_cast(camera.GetRotation()));
+		shader.setVec3("CamPosition", camera.Position);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.use();
+		shader.setUVec2("Resolution", windowWidth, windowHeight);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	windowWidth = width;
+	windowHeight = height;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		camera.ProcessKeyboard(UP, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+		camera.ProcessKeyboard(DOWN, deltaTime);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
