234c1c92fcad100766a7105775b319fb1a4a6880
ItayGigi <itay.gigi1@gmail.com>
2025-09-08 23:40:57 +0300

    updated opengl to 4.3

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ Renderer renderer;
 int main(int argc, const char* argv[]) {
 	// initialize glfw
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
